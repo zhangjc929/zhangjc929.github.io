2eb8d690938e52541ac9b7efc4aa99f3ebdf7345
Lkytal <lkytal@live.cn>
2015-02-07 00:10:10 +0800

    Fix defer

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function (){
+	$('#myTab a').click(function (e) {
+		e.preventDefault();
+		$(this).tab('show');
+	});
+});
+	
+var _hmt = _hmt || [];
+(function() {
+	var hm = document.createElement("script");
+	hm.src = "//hm.baidu.com/hm.js?86288502546a2fa398e94d0a198c2392";
+	var s = document.getElementsByTagName("script")[0];
+	s.parentNode.insertBefore(hm, s);
+})();

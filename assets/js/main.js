417341cdbf149a6f6e6d6add1f801ff7a852ac8f
Lkytal <lkytal@live.cn>
2015-02-07 00:22:35 +0800

    Re st

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,3 @@ $(document).ready(function (){
 		$(this).tab('show');
 	});
 });
-	
-var _hmt = _hmt || [];
-(function() {
-	var hm = document.createElement("script");
-	hm.src = "//hm.baidu.com/hm.js?86288502546a2fa398e94d0a198c2392";
-	var s = document.getElementsByTagName("script")[0];
-	s.parentNode.insertBefore(hm, s);
-})();
